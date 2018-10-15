405297db1259e5e70d60ab929befe7f0f0520555
Thanh Khuu <tkhuu5141@gmail.com>
2018-10-15 18:21:07 -0500

    Completed Get It Done Part 7 - Managing Login Using the Session

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,9 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = "secretkey"
+
+
 
 class Task(db.Model):
 
@@ -27,6 +30,13 @@ class User(db.Model):
         self.email = email
         self.password = password
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -35,6 +45,7 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and user.password == password:
             # TODO - "remember" that the user has logged in
+            session['email'] = email
             return redirect('/')
 
         else:
@@ -58,12 +69,18 @@ def register():
             db.session.add(new_user)
             db.session.commit()
             # TODO - "remember" the user
+            session['email'] = email
             return redirect("/")
         else:
             # TODO - user better response messaging
             return "<h1>Duplicate user</h1>"
 
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
